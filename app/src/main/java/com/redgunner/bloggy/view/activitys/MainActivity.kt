f0d642236e6c87a7f7fc8f2973bd85d854b11e8b
Youssef El Mountahi <yusuf.one@outlook.com>
2021-04-20 01:02:09 +0000

    Add pinch-to-zoom like Instagram to all image views

--- app/src/main/java/com/redgunner/bloggy/view/activitys/MainActivity.kt
+++ app/src/main/java/com/redgunner/bloggy/view/activitys/MainActivity.kt
@@ -2,9 +2,11 @@ package com.redgunner.bloggy.view.activitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import com.aghajari.zoomhelper.ZoomHelper
 import com.redgunner.bloggy.R
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
@@ -31,5 +33,10 @@ class MainActivity : AppCompatActivity() {
         bottom_navigation?.let {
             NavigationUI.setupWithNavController(it, navController)
         }
+    }
+
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        return ZoomHelper.getInstance().dispatchTouchEvent(ev!!,this) || super.dispatchTouchEvent(ev)
     }
 }
